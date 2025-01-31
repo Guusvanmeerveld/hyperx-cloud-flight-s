5efeea92c55252d4b7698b9036e975c1bcd2b2c6
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-01-31 20:17:02 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    naersk.url = "github:nix-community/naersk/master";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    utils,
+    naersk,
+    rust-overlay,
+    ...
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        naersk-lib = pkgs.callPackage naersk {};
+
+        runtimeDeps = with pkgs; [
+          hidapi
+        ];
+
+        buildDeps = with pkgs; [
+          pkg-config
+        ];
+      in {
+        defaultPackage = naersk-lib.buildPackage {
+          nativeBuildInputs = buildDeps;
+          buildInputs = runtimeDeps;
+
+          src = ./.;
+        };
+
+        devShell = pkgs.mkShell rec {
+          nativeBuildInputs = buildDeps;
+
+          buildInputs = with pkgs;
+            [
+              (rust-bin.stable.latest.default.override {
+                extensions = ["rust-src"];
+              })
+              rust-analyzer-unwrapped
+            ]
+            ++ runtimeDeps;
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (buildInputs ++ nativeBuildInputs);
+        };
+      }
+    );
+}
